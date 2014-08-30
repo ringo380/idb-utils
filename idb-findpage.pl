a98b1ba4dff60f22e6021097846de2b6806c9333
ringo380 <ringo380@gmail.com>
2014-08-30 16:48:03 -0500

    output

--- idb-findpage.pl
+++ idb-findpage.pl
@@ -87,7 +87,7 @@ if ($spaceid) {
 				my $db_name = $tblfile;
 				$db_name =~ s/.*$datadir\/(\w+)\/.*/\1/;
 				$table_name =~ s/.*$datadir\/$db_name\/(\w+)\.ibd.*/$1/;
-                print "Space ID $spaceid is associated with $tblfile\nSQL: $db_name.$table_name\n";
+                print "Space ID $spaceid is associated with $tblfile\n$db_name.$table_name\n";
                 exit;
             }
         }
